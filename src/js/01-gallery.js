a0d6dbe71d4ee95341230afd827f7b14f4b60137
bragynator <92689909+bragynator@users.noreply.github.com>
2022-04-27 00:25:57 +0300

    First task done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryRef.innerHTML = galleryMarkup;
+
+function createGalleryMarkup(items) {
+  return items
+    .map(
+      item => `
+                <a class="gallery__item" href="${item.original}">
+                    <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+                </a>`,
+    )
+    .join('');
+}
+
+let gallery = new SimpleLightbox('.gallery a', {
+  showCounter: false,
+  scaleImageToRatio: true,
+  maxZoom: 4,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+gallery.on('show.simplelightbox');
