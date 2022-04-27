99359c57680ed2a10e17608750bac394792ca928
bragynator <92689909+bragynator@users.noreply.github.com>
2022-04-28 00:06:44 +0300

    3rd task done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,11 +1,19 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
+import debounce from 'lodash.debounce';
 
 const player = new Player('vimeo-player');
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+player
+  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .catch(console.log('Play the video, please!!!'));
 player.on('timeupdate', throttle(saveCurrentTime, 2000));
+player.on('ended', debounce(deleteSavedTime, 2000));
 
 function saveCurrentTime(evt) {
   localStorage.setItem('videoplayer-current-time', evt.seconds);
+}
+
+function deleteSavedTime() {
+  localStorage.removeItem('videoplayer-current-time');
 }
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from 'lodash.throttle';
 
+const formRef = document.querySelector('.feedback-form');
+
+const {
+  elements: { email, message },
+} = formRef;
+
+updateFields();
+
+formRef.addEventListener('input', throttle(handleFormInput, 500));
+formRef.addEventListener('submit', handleSubmitBtn);
+
+function updateFields() {
+  const savedData = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+  email.value = savedData ? savedData.email : '';
+  message.value = savedData ? savedData.message : '';
+}
+
+function handleFormInput() {
+  const data = JSON.stringify({
+    email: email.value,
+    message: message.value,
+  });
+
+  localStorage.setItem('feedback-form-state', data);
+}
+
+function handleSubmitBtn(evt) {
+  evt.preventDefault();
+
+  console.log({
+    email: email.value,
+    message: message.value,
+  });
+
+  localStorage.removeItem('feedback-form-state');
+  evt.currentTarget.reset();
+}
