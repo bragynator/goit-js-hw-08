493a39b95d654b171bd5f0e601bd070d42234b5b
bragynator <92689909+bragynator@users.noreply.github.com>
2022-04-27 19:28:56 +0300

    Second task done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,11 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player('vimeo-player');
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+player.on('timeupdate', throttle(saveCurrentTime, 2000));
+
+function saveCurrentTime(evt) {
+  localStorage.setItem('videoplayer-current-time', evt.seconds);
+}
